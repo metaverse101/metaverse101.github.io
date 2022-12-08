f87e52dfde9febb31344125c80d9a8213cb0d143
Natalie <natalie.d.hsu@gmail.com>
2022-12-08 14:05:16 -0500

    Updating deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
 import vuetify from 'vite-plugin-vuetify'
 
+import { resolve } from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/metaverse/',
