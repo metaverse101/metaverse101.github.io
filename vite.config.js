e71d55261b117155fe2f31c1a0d881bed3fae0aa
Nat Hsu <natalie.d.hsu@gmail.com>
2022-12-13 08:16:11 -1000

    Updating config

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,9 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
-  plugins: [
-      vue(),
-      vuetify({ autoImport: true }),
-  ],
-})
+      base: '/',
+      plugins: [
+          vue(),
+          vuetify({ autoImport: true }),
+      ],
+    })
